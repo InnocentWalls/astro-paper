692fcd688fea0604cf55d3883cf869170b807871
InnocentWalls <97960685+InnocentWalls@users.noreply.github.com>
2024-02-10 16:28:53 +0900

    4

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "イット密入国者ブログ",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minute
 };
 
